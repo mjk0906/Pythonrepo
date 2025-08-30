ea46c3cd0445172cec1c5069d66767a661376d1a
Jayant Malka <rite2jayantkm@gmail.com>
2025-08-30 18:09:22 +0530

    new changes 1

--- code 2.py
+++ code 2.py
@@ -13,3 +13,16 @@ elif marks >= 40:
     print("D")
 else:
     print("So Sorry you are failed in the exam, BETTER LUCK NEXT TIME")
+
+    # this is a basic python code file 
+    #  this is added as a multi line comment 
+    num1=2
+    num2=5
+    sum= num1+num2
+    print("the sum of num1 and num2 is:",sum)
+
+    #relational operators 
+
+    num3=78
+    num4=73
+    print(num3<num4)
