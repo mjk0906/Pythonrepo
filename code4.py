563f84d695c0f98fe66d344662a84cc2a38c7b65
Jayant Malka <rite2jayantkm@gmail.com>
2025-08-31 22:54:50 +0530

    add changes

--- code4.py
+++ code4.py
@@ -4,3 +4,36 @@ print(len(name))
 
 print(name.endswith("n"))
 print(name.capitalize())
+name= name.capitalize()
+print(name)
+name=name.replace("a","o")
+print(name)
+print(name.find("o"))
+
+name1=str(input("enter your first name:"))
+print(len(name1))
+
+price=str(input("enter the price of your macbook:"))
+print(price.count("$"))
+#condotional statements
+
+
+
+    #nesting 
+age1=54
+if(age1>=18):
+    if(age1>=80):
+        print("you cannot drive due to poor senses")
+    else:
+        print("you can drive peacefully")
+elif(age1==17):
+    print("you can drive the upcoming year")
+else:
+        print("you cannot drive now")
+   
+number=int(input("enter any number:"))
+if (number%2==0):
+     print("the number entered by you is even")
+else:
+     print("the number you have entered is odd")
+     
---
+++ code5.py
@@ -0,0 +1,34 @@
+number=int(input("enter any number:"))
+if (number%2==0):
+     print("the number entered by you is even")
+else:
+     print("the number you have entered is odd")
+
+a=int(input("enter any number:"))
+b=int(input("enter any number:"))
+c=int(input("enter any number:"))
+if (a>b and a>c):
+     print("a is the greatest number")
+elif(b>c):
+     print("b is the greatest number")
+else:
+     print("c is the greatest number")
+
+d=int(input("enter the number 4:"))
+if (a>b and a>c and a>d):
+     print("the greatest number is a")
+elif(b>c and b>d):
+     print("the greatest number is b")
+elif(c>d):
+     print("the greatest number is c")
+else:
+     print("the greatest number is d")
+
+e=int(input("enter any number to check whether the number is a multiple of 7 or not "))
+if (e%7==0):
+     print("the number entered by you is multiple of 7")
+else:
+     print("the number entered by you is not a multiple of 7")
+
+
+     
