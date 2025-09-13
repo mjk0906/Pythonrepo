1cb4436a671a280e8941bff7e6f2532d9cbce7ed
Jayant Malka <rite2jayantkm@gmail.com>
2025-09-13 19:10:32 +0530

    add changes

--- code7.py
+++ code7.py
@@ -2,3 +2,17 @@ grade=["A","B","C","D","E","FAIL"]
 print(grade.count("A"))
 grade.sort(reverse=True)
 print(grade)
+
+#dictionary 
+info={
+    "name":"Jayant",
+    "age":18,
+    "college":"IIT",
+    "games":["cricket","BGMI","Call of Duty","Free Fire"],
+    "marks": 97.7 
+}
+
+print(info["games"])
+    
+
+
