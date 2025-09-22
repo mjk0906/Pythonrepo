0febf6e013d6f7c3b09a3a6a5b2467b06c92cc0f
Jayant Malka <rite2jayantkm@gmail.com>
2025-09-23 00:02:38 +0530

    add the changes

--- code7.py
+++ code7.py
@@ -41,3 +41,14 @@ while(count<=1):
     count+=1
 
 print("loop ended")     
+
+i=1
+while(i<=10):
+    print(3*i)
+    i+=1
+
+nums=[1,4,9,16,25,36,49,64,81,100]
+j=0
+while(j<=len(nums)):
+    print(nums[j])
+    j+=1
